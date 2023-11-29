8efe6ffc08e8c1e0ba01ea641862c1388d9eef1f
chrisIyer <hellohotbox420@gmail.com>
2023-11-29 14:52:19 -0500

    layout coming along.

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,14 +4,14 @@ import UploadWidget from './UploadWidget';
 import '../App.css'
 
 export default function Image() {
-     const [foundImages, setFoundImages]=useState(null)
-     const [images, setImages] = useState([])
-     const [image, setImage] = useState({
-          link: '', 
-          alt: '', 
-          likes: 0
-})
-const [showInput, setShowInput] = useState(false)
+  const [foundImages, setFoundImages] = useState(null)
+  const [images, setImages] = useState([])
+  const [image, setImage] = useState({
+    link: '',
+    alt: '',
+    likes: 0
+  })
+  const [showInput, setShowInput] = useState(false)
   const [url, updateUrl] = useState(false);
   const [error, updateError] = useState();
   const inputRef = useRef(null)
@@ -20,212 +20,211 @@ const [showInput, setShowInput] = useState(false)
   }
 
   const getImages = async () => {
-     try {
-       const response = await fetch('/api/images')
-       const data = await response.json()
-       setImages(data)
-       console.log(data)
-     } catch (error) {
-       console.error(error)
-     }
-   }
- 
-   const createImage = async () => {
-     try {
-       const response = await fetch('/api/images', {
-         method: "POST",
-         headers: {
-           'Content-Type': 'application/json'
-         },
-         body: JSON.stringify({ ...image })
-       })
-       const data = await response.json()
-       setFoundImages(data)
-       setImage({
-          link: '', 
-          alt: '', 
-          likes: 0
-       })
-     } catch (error) {
-       console.error(error)
-     }
-   }
- 
- 
-   const deleteImage = async (id) => {
-     try {
-       const response = await fetch(`/api/images/${id}`, {
-         method: 'DELETE',
-         headers: {
-           'Content-Type': 'application/json',
-         }
-       })
-       const data = await response.json()
-       // const imagesCopy = [...images]
-       // const index = imagesCopy.findIndex(image => id === image._id)
-       // imagesCopy.splice(index, 1)
-       // setImages(imagesCopy)
-       setFoundImages(data)
-     } catch (error) {
-       console.error(error)
-     }
-   }
-   const updateImage = async (id, updatedData) => {
-     try {
-       const response = await fetch(`/api/images/${id}`, {
-         method: 'PUT',
-         headers: {
-           'Content-Type': 'application/json',
-         },
-         body: JSON.stringify(updatedData)
-       })
-       const data = await response.json()
-       setFoundImages(data)
-       const imagesCopy = [...images]
-       const index = imagesCopy.findIndex(image => id === image._id)
-       imagesCopy[index] = { ...imagesCopy[index], ...updatedData }
-       setImages(imagesCopy)
-     } catch (error) {
-       console.error(error)
-     }
-   }
- 
-   const likeImage = async (id) => {
-     try {
-       const index = images.findIndex((image) => image._id === id)
-       const imagesCopy = [...images]
-       const subject = imagesCopy[index]
-       subject.likes = subject.likes + 1
-       const response = await fetch(`/api/images/${id}`, {
-         method: 'PUT',
-         headers: {
-           'Content-Type': 'application/json'
-         },
-         body: JSON.stringify(subject)
-       })
-       const updatedImage = await response.json()
-       const completedImagesCopy = [updatedImage, ...images]
- 
-       setImages(completedImagesCopy)
-       // imagesCopy.splice(index, 1)
-       setImages(imagesCopy)
- 
-     } catch (error) {
-       console.error(error)
-     }
-   }
-   useEffect(() => {
-     getImages()
-   }, [foundImages])
- 
-   function handleOnUpload(error, result, widget) {
-     if (error) {
-       updateError(error);
-       widget.close({
-         quiet: true
-       });
-       return;
-     }
-     console.dir(result);
-     updateUrl(result?.info?.secure_url);
-     console.dir(url);
-     setImage({
-          link: result?.info?.secure_url, 
-          alt: '', 
-          likes: 0
-     })
- }
- return (
-     <>
- 
-       <section>
-         <h1>Lorelei's Art</h1>
-         <div>
-           <span>
-             <UploadWidget onUpload={handleOnUpload}>
-               {({ open }) => {
-                 function handleOnClick(e) {
-                   e.preventDefault();
-                   open();
-                 }
-                 return (
-                   <button style={{ "backgroundColor": 'rgba(162, 134, 109, 0.5)' , 'marginBottom': "19px", 'borderBottomColor': 'red', 'height':'19px'}} onClick={handleOnClick}></button>
-                 )
-               }}
-             </UploadWidget>
-             {error && <p>{error}</p>}
-             {url && (
-            
-               <div key={url._id} className='card' style={{ width: '8rem', 'marginBottom': '1px', 'backgroundColor': 'red' }}>
-                 <img variant="top" src={url} alt='uploaded image' id="uploadedimage" style={{ 'width': 90, "borderRadius": "5%" }}></img>
+    try {
+      const response = await fetch('/api/images')
+      const data = await response.json()
+      setImages(data)
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-               </div>
-             )}
-           </span>
- 
-           <br></br>
-           {/* <input
+  const createImage = async () => {
+    try {
+      const response = await fetch('/api/images', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ...image })
+      })
+      const data = await response.json()
+      setFoundImages(data)
+      setImage({
+        link: '',
+        alt: '',
+        likes: 0
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
+  const deleteImage = async (id) => {
+    try {
+      const response = await fetch(`/api/images/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      const data = await response.json()
+      // const imagesCopy = [...images]
+      // const index = imagesCopy.findIndex(image => id === image._id)
+      // imagesCopy.splice(index, 1)
+      // setImages(imagesCopy)
+      setFoundImages(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const updateImage = async (id, updatedData) => {
+    try {
+      const response = await fetch(`/api/images/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData)
+      })
+      const data = await response.json()
+      setFoundImages(data)
+      const imagesCopy = [...images]
+      const index = imagesCopy.findIndex(image => id === image._id)
+      imagesCopy[index] = { ...imagesCopy[index], ...updatedData }
+      setImages(imagesCopy)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const likeImage = async (id) => {
+    try {
+      const index = images.findIndex((image) => image._id === id)
+      const imagesCopy = [...images]
+      const subject = imagesCopy[index]
+      subject.likes = subject.likes + 1
+      const response = await fetch(`/api/images/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(subject)
+      })
+      const updatedImage = await response.json()
+      const completedImagesCopy = [updatedImage, ...images]
+
+      setImages(completedImagesCopy)
+      // imagesCopy.splice(index, 1)
+      setImages(imagesCopy)
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    getImages()
+  }, [foundImages])
+
+  function handleOnUpload(error, result, widget) {
+    if (error) {
+      updateError(error);
+      widget.close({
+        quiet: true
+      });
+      return;
+    }
+    console.dir(result);
+    updateUrl(result?.info?.secure_url);
+    console.dir(url);
+    setImage({
+      link: result?.info?.secure_url,
+      alt: '',
+      likes: 0
+    })
+  }
+  return (
+    <>
+      <div className='canvas'>
+        <h1>Lorelei's Art</h1>
+        <div className='uploadForm'>
+          
+          <div>
+            <span>
+              <UploadWidget onUpload={handleOnUpload}>
+                {({ open }) => {
+                  function handleOnClick(e) {
+                    e.preventDefault();
+                    open();
+                  }
+                  return (
+                    <button style={{ "backgroundColor": 'rgba(162, 134, 109, 0.5)', 'marginBottom': "19px", 'borderBottomColor': 'red', 'height': '19px' }} onClick={handleOnClick}>Upload Photo</button>
+                  )
+                }}
+              </UploadWidget>
+              {error && <p>{error}</p>}
+              {url && (
+
+                <div key={url._id} className='card' style={{ width: '8rem', 'marginBottom': '1px', 'backgroundColor': 'red' }}>
+                  <img variant="top" src={url} alt='uploaded image' id="uploadedimage" style={{ 'width': 90, "borderRadius": "5%" }}></img>
+
+                </div>
+              )}
+            </span>
+
+           
+            <input
              type='text'
              src={image.link}
              onChange={handleChange}
              name="link"
              placeholder='Link'
            >
-           </input> */}
-           <br />
-           <input
-             value={image.alt}
-             onChange={handleChange}
-             name="alt"
-             placeholder='Alt Text'>
            </input>
-           <br />
-           
-           
-           <button onClick={() => createImage()}>Display your Entry</button>
-         </div>
-       </section>
-       <hr></hr>
-       {images && images.length ? (
-         <div className='collumns'>
-           {images.map((image) => {
-             return (
-               <section key={image._id} className="w-75 p-3">
-                
-                     <img style={{ "maxWidth": "100%", "height": "15vw" }} src={image.link} alt={image.alt} />
-                  
-                     <div>
-                       {/* <img src={image.link} alt={image.alt}></img> */}
-                       <h2 onClick={() => setShowInput(!showInput)}>{image.alt}
-                         <input
-                           ref={inputRef}
-                           style={{ display: showInput ? 'block' : 'none' }}
-                           type='text'
-                           onKeyDown={(e) => {
-                             if (e.key === 'Enter') {
-                               e.preventDefault()
-                               // const text = inputRef.current.value
-                               updateImage(image._id, { alt: e.target.value })
-                               setShowInput(false)
-                               
-                             }
-                           }}
-                           defaultValue='...'
-                         />
-                       </h2>
-                 
-                       <button style={{ 'fontStyle': 'italic' }} className="btn btn-outline-warning" onClick={() => likeImage(image._id)}> {image.likes}</button>
-                     </div>
-               
-               </section>
- 
-             )
-           }
-           )
-           }
-         </div>) : <>No Entries yet! Yet Add One Below this message</>
-       }
-     </>
- 
-   )
+
+            <input
+              value={image.alt}
+              onChange={handleChange}
+              name="alt"
+              placeholder='Alt Text'>
+            </input>
+ <button onClick={() => createImage()}>Display your Entry</button>
+          </div>
+        </div>
+        <hr></hr>
+        {images && images.length ? (
+          <div className='collumns'>
+            {images.map((image) => {
+              return (
+                <section key={image._id} className="w-75 p-3">
+
+                  <img style={{ "maxWidth": "100%", "height": "15vw" }} src={image.link} alt={image.alt} />
+
+                  <div>
+                    {/* <img src={image.link} alt={image.alt}></img> */}
+                    <p onClick={() => setShowInput(!showInput)}>{image.alt}
+                      <input
+                        ref={inputRef}
+                        style={{ display: showInput ? 'block' : 'none' }}
+                        type='text'
+                        onKeyDown={(e) => {
+                          if (e.key === 'Enter') {
+                            e.preventDefault()
+                            // const text = inputRef.current.value
+                            updateImage(image._id, { alt: e.target.value })
+                            setShowInput(false)
+
+                          }
+                        }}
+                        defaultValue='...'
+                      />
+                    </p>
+
+                    <button style={{ 'fontStyle': 'italic' }} className="btn btn-outline-warning" onClick={() => likeImage(image._id)}> {image.likes}</button>
+                  </div>
+
+                </section>
+
+              )
+            }
+            )
+            }
+          </div>) : <>No Entries yet! Yet Add One Below this message</>
+        }
+      </div>
+    </>
+
+  )
 }
