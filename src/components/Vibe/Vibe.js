466bafc63718ee0890b7a3a1557be23c187545a8
chrisIyer <hellohotbox420@gmail.com>
2024-01-03 18:02:59 -0500

    made a text area

--- src/components/Vibe/Vibe.js
+++ src/components/Vibe/Vibe.js
@@ -200,8 +200,8 @@ export default function Vibe() {
             value={vibe.text}
             onChange={handleChange}
             // name="textarea"
-            rows={18}
-            cols={80}
+            rows={8}
+            cols={100}
             style={{borderRadius:'5%', outline: 'dotted', color: 'pink'}}
             placeholder='Some meaningful text'>
           </textarea>
