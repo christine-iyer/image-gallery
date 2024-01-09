1f7ce2ac320b4ba88ac61b2b6a4f5ff7c8e47ba9
chrisIyer <hellohotbox420@gmail.com>
2024-01-09 17:08:58 -0500

    broke down create .

--- src/components/Blocks/CreateBlock.js
+++ src/components/Blocks/CreateBlock.js
@@ -1,70 +1,49 @@
-
-export default function CreateBlock ({
-  createBlock,
-  block,
-  handleChange
-}
-) {
-  return (
-    <div >
-      <h2 >Create A Block</h2>
-      <div >
-        <form 
-          onSubmit={(e) => {
-            e.preventDefault()
-            createBlock()
-          }}
-        >
-          
-          <label>Title
-            <input 
-            type='text' 
-            value={block.title} 
-            name='title' 
-            onChange={handleChange} 
-            placeholder='Title' 
-            />
-          </label>
-          <label>Author
-            <input 
-            type='text' 
-            value={block.author} 
-            name='author' 
-            onChange={handleChange} 
-            placeholder='Author' 
-            />
-          </label>
-
-          <label>Pick a category:
-            <select 
-            options={block.category} 
-            value={block.category} 
-            onChange={handleChange} 
-            placeholder='Category'
+export default function CreateBlock({vibe, handleChange}){
+  return(
+    <>
+    <input
+            type='text'
+            value={vibe.title}
+            onChange={handleChange}
+            name="title"
+            placeholder='Title'
+          >
+          </input>
+          <br />
+          <input
+            value={vibe.author}
+            onChange={handleChange}
+            name="author"
+            placeholder='Author'>
+          </input>
+          <br />
+          <textarea
+            value={vibe.text}
+            onChange={handleChange}
+            // name="textarea"
+            rows={8}
+            cols={100}
+            style={{ borderRadius: '5%', outline: 'dotted', color: 'pink' }}
+            placeholder='Some meaningful text'>
+          </textarea>
+          <br />
+          <select
+            value={vibe.category}
+            onChange={handleChange}
             name="category">
-              <option value="Family">Family</option>
-              <option  value="Friends">Friends</option>
-              <option  value="Work">Work</option>
-              <option  value="Code">Code</option>
-              <option  value="Misc">Misc</option>
-            </select>
-            </label>
-
-
-          <label>Text
-            <input 
-            type='text' 
-            value={block.text} 
-            name='text' 
-            onChange={handleChange} 
-            placeholder='Text' />
-          </label>
- <input  
-          type='submit' 
-          value='Create Block' />
-</form>
-      </div>
-     
-    </div>
+            <option value="Misc">Select a 🤍</option>
+            <option value="#Beer">#Beer</option>
+            <option value="#Weed">#Weed</option>
+            <option value="#Bars">#Bars</option>
+            <option value="#Outdoors">#Outdoors</option>
+            <option value="#Kids">#Kids</option>
+            <option value="#Indoors">#Indoors</option>
+          </select>
+          <br />
+          <br />
+          <button onClick={() => createVibe()}>Display your Entry</button>
+      
+    
+    </>
   )
 }
