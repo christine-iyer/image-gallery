0f43d1bdd938b7bd34258d57fd4108e38f009b7e
chrisIyer <hellohotbox420@gmail.com>
2024-01-10 10:29:55 -0500

    did Block.js, and tried just gutting it. We'll see how it goes.

---
+++ src/components/Blocks/Block.js
@@ -0,0 +1,46 @@
+export default function Block({}){
+     return(
+          <div>
+                <MDBCard key={vibe._id} className="w-75 p-3">
+               
+                    <MDBCardImage style={{ "maxWidth": "100%", "height": "15vw" }} src={vibe.image} alt='...' fluid />
+
+                    <MDBCardBody>
+                      <MDBCardTitle>{vibe.title}</MDBCardTitle>
+                      <MDBCardText key={vibe.id} onClick={() => setShowInput(!showInput)}>
+                        {readMore ? vibe.text : `${vibe.text.substring(0, 38)}...`}
+                        <button
+                          style={{ color: 'greenyellow' }}
+                          className="btn"
+                          onClick={() => setReadMore(!readMore)}>
+                        </button>
+                        <input
+                          ref={inputRef}
+                          style={{ display: showInput ? 'block' : 'none' }}
+                          type='text'
+                          onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                              e.preventDefault()
+                              // const text = inputRef.current.value
+                              updateVibe(vibe._id, { text: e.target.value })
+                              setShowInput(false)
+                            }
+                          }}
+                          defaultValue={vibe.text}
+                        />
+                      </MDBCardText>
+                      <MDBCardText>
+                        <small className='text-muted'>
+                          {vibe.author} posted on {new Date(vibe.createdAt).toLocaleDateString()}
+                        </small>
+                      </MDBCardText>
+                      <button style={{ 'fontStyle': 'italic' }} className="btn btn-outline-warning" onClick={() => likeVibe(vibe._id)}>♥️ {vibe.like}</button>
+                      <br></br>
+                      {vibe.category}
+                    </MDBCardBody>
+
+              </MDBCard>           
+
+          </div>
+     )
+}
