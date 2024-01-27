78f9c4ead3be008dd0c671a53eed62a02a16951a
chrisIyer <hellohotbox420@gmail.com>
2024-01-26 21:23:02 -0500

    working on it.

--- src/components/Vibe/Vibe.js
+++ src/components/Vibe/Vibe.js
@@ -85,18 +85,17 @@ export default function Vibe() {
       console.error(error)
     }
   }
-
   const updateVibe = async (id, updatedData) => {
     try {
       const response = await fetch(`/api/vibes/${id}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
+      
         },
         body: JSON.stringify(updatedData)
       })
       const data = await response.json()
-      setFoundVibes(data)
       const vibesCopy = [...vibes]
       const index = vibesCopy.findIndex(vibe => id === vibe._id)
       vibesCopy[index] = { ...vibesCopy[index], ...updatedData }
@@ -105,6 +104,26 @@ export default function Vibe() {
       console.error(error)
     }
   }
+
+  // const updateVibe = async (id, updatedData) => {
+  //   try {
+  //     const response = await fetch(`/api/vibes/${id}`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(updatedData)
+  //     })
+  //     const data = await response.json()
+  //     setFoundVibes(data)
+  //     const vibesCopy = [...vibes]
+  //     const index = vibesCopy.findIndex(vibe => id === vibe._id)
+  //     vibesCopy[index] = { ...vibesCopy[index], ...updatedData }
+  //     setVibes(vibesCopy)
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
 
   const likeVibe = async (id) => {
     try {
@@ -228,52 +247,75 @@ export default function Vibe() {
         <Container className='collumns'>
           {vibes.map((vibe) => {
             return (
-              <MDBCard key={vibe._id} className="w-75 p-3">
-                <MDBRow className='g-0'>
-                  <MDBCol md='4'>
-                    <MDBCardImage style={{ "maxWidth": "100%", "height": "15vw" }} src={vibe.image} alt='...' fluid />
-                  </MDBCol>
-                  <MDBCol md='8'>
-                    <MDBCardBody>
-                      <MDBCardTitle>{vibe.title}</MDBCardTitle>
-                      <MDBCardText key='id' onClick={() => setShowInput(!showInput)}>
-                      {vibe.text}
-                        <button
-                          style={{ color: 'greenyellow' }}
-                          className="btn"
-                          onClick={() => setReadMore(!readMore)}>
-                        </button>
-                        <input
-                          ref={inputRef}
-                          style={{ display: showInput ? 'block' : 'none' }}
-                          type='text'
-                          onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                              // e.preventDefault()
-                              // const text = inputRef.current.value
-                              updateVibe(vibe._id, { text: e.target.value })
-                              setShowInput(!setShowInput)
-                            }
-                          }}
-                          defaultValue={vibe.text}
-                        />
-                        <button   onClick={() => updateVibe(vibe._id)}
+
+        //       <MDBCard key={vibe._id} className="w-75 p-3">
+        //         <MDBRow className='g-0'>
+        //           <MDBCol md='4'>
+        //             <MDBCardImage style={{ "maxWidth": "100%", "height": "15vw" }} src={vibe.image} alt='...' fluid />
+        //           </MDBCol>
+        //           <MDBCol md='8'>
+        //             <MDBCardBody>
+        //               <MDBCardTitle>{vibe.title}</MDBCardTitle>
+        //               <MDBCardText key='id' onClick={() => setShowInput(!showInput)}>
+        //               {vibe.text}
+        //                 <button
+        //                   style={{ color: 'greenyellow' }}
+        //                   className="btn"
+        //                   onClick={() => setReadMore(!readMore)}>
+        //                 </button>
+        //                 <input
+        //                   ref={inputRef}
+        //                   style={{ display: showInput ? 'block' : 'none' }}
+        //                   type='text'
+        //                   onKeyDown={(e) => {
+        //                     if (e.key === 'Enter') {
+        //                       // e.preventDefault()
+        //                        const text = inputRef.current.value
+        //                       updateVibe(vibe._id, { text })
+        //                       setShowInput(false)
+        //                     }
+        //                   }}
+        //                   defaultValue={vibe.text}
+        //                 />
+        //                 <button   onClick={() => updateVibe(vibe._id)}
+        // >
+        //   Edit Me
+        // </button>
+        //               </MDBCardText> 
+        //               <MDBCardText>
+        //                 <small className='text-muted'>
+        //                   {vibe.author} posted on {new Date(vibe.createdAt).toLocaleDateString()}
+        //                 </small>
+        //               </MDBCardText>
+        //               <button style={{ 'fontStyle': 'italic' }} className="btn btn-outline-warning" onClick={() => likeVibe(vibe._id)}>♥️ {vibe.like}</button>
+        //               <br></br>
+        //               {vibe.category}
+        //             </MDBCardBody>
+        //           </MDBCol>
+        //         </MDBRow>
+        //       </MDBCard>
+        <li>
+        <h4 onClick={() => setShowInput(!showInput)}>{vibe.title}</h4>
+        <input
+          ref={inputRef}
+          style={{ display: showInput ? 'block' : 'none' }}
+          type='text'
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              const title = inputRef.current.value
+              updateVibe(vibe._id, { title })
+              setShowInput(false)
+            }
+          }}
+          defaultValue={vibe.title}
+        />
+        <a href={vibe.url} target='_blank' rel='noreferrer'> {vibe.url}</a>
+        <button
+          onClick={() => deleteVibe(vibe._id)}
         >
-          Edit Me
+          Delete Me
         </button>
-                      </MDBCardText> 
-                      <MDBCardText>
-                        <small className='text-muted'>
-                          {vibe.author} posted on {new Date(vibe.createdAt).toLocaleDateString()}
-                        </small>
-                      </MDBCardText>
-                      <button style={{ 'fontStyle': 'italic' }} className="btn btn-outline-warning" onClick={() => likeVibe(vibe._id)}>♥️ {vibe.like}</button>
-                      <br></br>
-                      {vibe.category}
-                    </MDBCardBody>
-                  </MDBCol>
-                </MDBRow>
-              </MDBCard>
+      </li>
             )
           }
           )
